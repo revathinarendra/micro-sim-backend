e87a3f8870402d90fff939220e7b440f7905a6ae
KATARI REVATHI <katari.revathi@gmail.com>
2025-04-05 22:13:26 +0530

    wiki url dic

--- flashcard/views.py
+++ flashcard/views.py
@@ -93,24 +93,59 @@ class WikipediaDetailView(RetrieveAPIView):
 
 # post method to save the front data to the wikipedia table
 
+# @api_view(['POST'])
+# def save_wikipedia(request):
+#     wikipedia_url = request.data.get("wikipedia_url")
+    
+#     if not wikipedia_url:
+#         return Response({"error": "Wikipedia URL is required"}, status=status.HTTP_400_BAD_REQUEST)
+    
+#     # Try getting an existing object
+#     wikipedia_obj = Wikipedia.objects.filter(wikipedia_url=wikipedia_url).first()
+    
+#     if wikipedia_obj:
+#         serializer = WikipediaSerializer(wikipedia_obj, data=request.data, partial=True)
+#     else:
+#         # Create a slug from the URL if not provided
+#         if 'slug' not in request.data or not request.data['slug']:
+#             request.data['slug'] = slugify(wikipedia_url.split('/')[-1])
+#         serializer = WikipediaSerializer(data=request.data)
+    
+#     if serializer.is_valid():
+#         wikipedia_instance = serializer.save()
+#         return Response({
+#             "message": "Wikipedia entry saved", 
+#             "id": wikipedia_instance.id,
+#             "data": serializer.data
+#         }, status=status.HTTP_201_CREATED)
+    
+#     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
 @api_view(['POST'])
 def save_wikipedia(request):
-    wikipedia_url = request.data.get("wikipedia_url")
-    
-    if not wikipedia_url:
-        return Response({"error": "Wikipedia URL is required"}, status=status.HTTP_400_BAD_REQUEST)
-    
+    wikipedia_url_data = request.data.get("wikipedia_url")
+
+    # Case where frontend sends it as {"url": "https://en.wikipedia.org/xyz"}
+    if isinstance(wikipedia_url_data, dict):
+        wikipedia_url = wikipedia_url_data.get("url")
+    else:
+        wikipedia_url = wikipedia_url_data
+
+    if not wikipedia_url or not isinstance(wikipedia_url, str):
+        return Response({"error": "Invalid or missing wikipedia_url."}, status=status.HTTP_400_BAD_REQUEST)
+
     # Try getting an existing object
     wikipedia_obj = Wikipedia.objects.filter(wikipedia_url=wikipedia_url).first()
-    
+
     if wikipedia_obj:
         serializer = WikipediaSerializer(wikipedia_obj, data=request.data, partial=True)
     else:
-        # Create a slug from the URL if not provided
         if 'slug' not in request.data or not request.data['slug']:
             request.data['slug'] = slugify(wikipedia_url.split('/')[-1])
+        request.data['wikipedia_url'] = wikipedia_url  # Make sure it's the cleaned string
         serializer = WikipediaSerializer(data=request.data)
-    
+
     if serializer.is_valid():
         wikipedia_instance = serializer.save()
         return Response({
@@ -118,7 +153,5 @@ def save_wikipedia(request):
             "id": wikipedia_instance.id,
             "data": serializer.data
         }, status=status.HTTP_201_CREATED)
-    
+
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
